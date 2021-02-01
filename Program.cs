b160980da204e9abba88035c2cb84ae520a56cf5
Jonathan Bauer <jonathan.bauer96@googlemail.com>
2021-02-01 14:58:07 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DotNET5TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello World!");
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
